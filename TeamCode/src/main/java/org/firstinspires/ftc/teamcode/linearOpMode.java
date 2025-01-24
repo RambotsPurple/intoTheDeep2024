aed0e584d5e93e33a681c919177388b51e011f99
luke <lukezf2@gmail.com>
2025-01-24 14:11:23 -0800

    n23

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearOpMode.java
@@ -141,24 +141,19 @@ public class linearOpMode extends LinearOpMode {
         backRightPower /= power - turn;
       }
 
-      // if A on the controller is pressed it will check if the claw is closed
-      // HOTFIX: A is open, B is close
+      // toggle claw
       if (gamepad2.a && intakeReleased) {
-        intakePower = (intakePower == 1 ? 0 : 1);
+        intakePower = 1 - intakePower;
         intakeReleased = false;
+      } else if(!gamepad2.a) {
+        intakeReleased = true;
       }
 
       // increment intake pos
-      else if (gamepad2.b && intakeReleased) {
+      if (gamepad2.b) {
         intakePower = Math.max(intakePower, intakePower - 0.1);
-        intakeReleased = false;
-      } else if (gamepad2.y && intakeReleased) {
+      } else if (gamepad2.y) {
         intakePower = Math.min(intakePower, intakePower + 0.1);
-        intakeReleased = false;
-      }
-
-      if(!gamepad2.a && !gamepad2.b) {
-        intakeReleased = true;
       }
 
       // presets
@@ -197,19 +192,19 @@ public class linearOpMode extends LinearOpMode {
         slideAbduction2.setPower(-slideAbdPower * 0.65);
       } // if else
 
-      slideExtension.setPower(-slideExtendPower);
+      slideExtension.setPower(slideExtendPower);
 
       // Wrist power
       wrist1.setPosition(wristPos);
-      if (gamepad2.left_trigger > 0) {
-        wristPos = Math.min(1, wristPos + 0.1);
+      if (gamepad2.left_trigger > 0.9) {
+        wristPos = Math.min(1, wristPos + 0.05);
       } else if (gamepad2.left_bumper) {
-        wristPos = Math.max(0, wristPos - 0.1);
+        wristPos = Math.max(0, wristPos - 0.05);
       }
 
       // Power to the intake
       leftIntake.setPosition(intakePower);
-      rightIntake.setPosition(intakePower - (1 - intakePower));
+      rightIntake.setPosition(1 - intakePower);
 
       // Telemetry
       telemetry.addData("RUNNING PRESET:", runningPreset);
@@ -218,7 +213,7 @@ public class linearOpMode extends LinearOpMode {
       telemetry.addData("Abd 1 position:", slideAbduction.getCurrentPosition());
       telemetry.addData("Abd 2 position:", slideAbduction2.getCurrentPosition());
       telemetry.addData("Ext position:", slideExtension.getCurrentPosition());
-      telemetry.addData("wrist pos:", wrist1);
+      telemetry.addData("wrist pos:", wrist1.getPosition());
       telemetry.addData("X", x);
       telemetry.addData("Y", y);
       telemetry.addData("Alpha", sensor.alpha());
