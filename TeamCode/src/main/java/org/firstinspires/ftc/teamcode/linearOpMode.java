23b9a6215736f05da47f04861617fffd5e65e820
deripy <151796113+der1py@users.noreply.github.com>
2025-01-24 14:48:19 -0800

    goat bug ciudxcsdds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearOpMode.java
@@ -202,9 +202,9 @@ public class linearOpMode extends LinearOpMode {
       // Wrist power
       wrist1.setPosition(wristPos);
       if (gamepad2.left_trigger > 0) {
-        wristPos = Math.min(1, wristPos + 0.1);
+        wristPos = Math.min(1, wristPos + 0.035);
       } else if (gamepad2.left_bumper) {
-        wristPos = Math.max(0, wristPos - 0.1);
+        wristPos = Math.max(0, wristPos - 0.035);
       }
 
       // Power to the intake
