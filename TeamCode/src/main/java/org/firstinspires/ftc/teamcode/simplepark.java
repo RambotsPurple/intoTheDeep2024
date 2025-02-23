fcae9adbff9b2109e01e57305ffe4deb445e472e
deripy <151796113+der1py@users.noreply.github.com>
2025-02-23 09:29:35 -0800

    Update simplepark.java
    sigma sigma on the wall

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simplepark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/simplepark.java
@@ -1,1 +1,31 @@
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@Autonomous(name = "Simple park pls hardstop")
+public class simplepark extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        // initializing hardware
+        RobotConfig.initialize(hardwareMap);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+        double pow = -0.5;
+        RobotConfig.frontLeft.setPower(pow);
+        RobotConfig.rearLeft.setPower(pow);
+        RobotConfig.frontRight.setPower(pow);
+        RobotConfig.rearRight.setPower(pow);
+
+        }
+    }
+
+}
