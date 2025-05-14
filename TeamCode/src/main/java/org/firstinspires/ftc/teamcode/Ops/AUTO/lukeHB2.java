deea09d8ad9f1d3dfbe2eb0e95a13b692a04f2c0
mdss jr team <mtdouglas.robotics@gmail.com>
2025-05-14 11:44:38 -0700

    please no merge conf

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ops/AUTO/lukeHB2.java
@@ -0,0 +1,298 @@
+package org.firstinspires.ftc.teamcode.Ops.AUTO;
+
+import androidx.annotation.NonNull;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.robot.Robot;
+
+import org.firstinspires.ftc.teamcode.RobotConfig;
+import org.firstinspires.ftc.teamcode.RR.SparkFunOTOSDrive;
+
+@Config
+
+@Autonomous(name = "real high basket pls work 5/13/25", group = "Autonomous")
+public class lukeHB2 extends LinearOpMode {
+    int targetPos = 0;
+
+    public class wrist{
+        public wrist () {
+            RobotConfig.wrist1.setPosition(0.8);
+        }
+        public class WristUp implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                RobotConfig.wrist1.setPosition(0.25);
+                return false;
+            }
+        }
+        public  Action wristUp() {
+            return new WristUp();
+        }
+
+        public class WristDown implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                RobotConfig.wrist1.setPosition(0.8);
+                return false;
+            }
+        }
+        public Action wristDown() {
+            return new WristDown();
+        }
+
+    }
+
+    public class Lift {
+
+        // component
+        public Lift() {
+            RobotConfig.arm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            RobotConfig.arm1.setDirection(DcMotorSimple.Direction.FORWARD);
+
+            RobotConfig.arm2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            RobotConfig.arm2.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+
+        // raises lift
+        public class LiftUp implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                telemetry.addLine("running lift");
+                telemetry.update();
+                if (!initialized) {
+                    targetPos = RobotConfig.ABD_SPEC;
+                    RobotConfig.arm1.setTargetPosition(targetPos);
+                    RobotConfig.arm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    RobotConfig.arm1.setVelocity(1000);
+                    RobotConfig.arm2.setTargetPosition(targetPos);
+                    RobotConfig.arm2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    RobotConfig.arm2.setVelocity(1000);
+                    initialized = true;
+                }
+
+                double pos = RobotConfig.arm1.getCurrentPosition();
+                packet.put("liftPos", pos);
+                if (pos < RobotConfig.ABD_SPEC) {
+                    return true;
+                } else {
+                    targetPos = 0;
+                    RobotConfig.arm1.setPower(targetPos);
+                    RobotConfig.arm2.setPower(targetPos);
+                    return false;
+                }
+            }
+        }
+
+        // function for raising lift
+        public Action liftUp() {
+            return new LiftUp();
+        }
+
+
+        // pickup
+        public class LiftDown implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    targetPos = RobotConfig.ABD_PICKUP;
+                    RobotConfig.arm1.setTargetPosition(targetPos);
+                    RobotConfig.arm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    RobotConfig.arm1.setVelocity(1000);
+                    RobotConfig.arm2.setTargetPosition(targetPos);
+                    RobotConfig.arm2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    RobotConfig.arm2.setVelocity(1000);
+                    initialized = true;
+                }
+
+                double pos = RobotConfig.arm1.getCurrentPosition();
+                packet.put("liftPos", pos);
+                if (pos > RobotConfig.ABD_PICKUP) {
+                    return true;
+                } else {
+                    targetPos = 0;
+                    RobotConfig.arm1.setPower(targetPos);
+                    RobotConfig.arm2.setPower(targetPos);
+                    return false;
+                }
+            }
+        }
+
+        // lower lift
+        public Action liftDown(){
+            return new LiftDown();
+        }
+    }
+
+    // Claw component
+    public class Claw {
+
+        public Claw() {
+            RobotConfig.intake.setPosition(1.0);
+        }
+
+        public class CloseClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                RobotConfig.intake.setPosition(0.55);
+                return false;
+            }
+        }
+        public Action closeClaw() {
+            return new CloseClaw();
+        }
+
+        public class OpenClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                RobotConfig.intake.setPosition(1.0);
+                return false;
+            }
+        }
+        public Action openClaw() {
+            return new OpenClaw();
+        }
+    }
+    public  class extend {
+
+        public class extendForward implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                telemetry.addLine("running ext");
+                telemetry.update();
+                if (!initialized) {
+//                    targetPos = RobotConfig.EXT_BASKET;
+//                    RobotConfig.slideExtension.setTargetPosition(targetPos);
+//                    RobotConfig.slideExtension.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//                    RobotConfig.slideExtension.setVelocity(1000);
+
+                    targetPos = RobotConfig.EXT_BASKET; //make it full slide extend
+                    RobotConfig.slideExtension.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    while (RobotConfig.slideExtension.getCurrentPosition() > targetPos) {
+                        RobotConfig.slideExtension.setPower(-1);
+                        telemetry.addData("ext pos:", RobotConfig.slideExtension.getCurrentPosition());
+                        telemetry.update();
+                    }
+                    initialized = true;
+//                    RobotConfig.slideExtension.setPower(0);
+                }
+
+                double pos = RobotConfig.slideExtension.getCurrentPosition();
+                packet.put("liftPos", pos);
+                if (pos < RobotConfig.EXT_BASKET) {
+                    return true;
+                } else {
+                    RobotConfig.slideExtension.setPower(targetPos);
+                    RobotConfig.slideExtension.setPower(targetPos);
+                    return false;
+                }
+            }
+        }
+        public Action extendForward() {
+            return new extend.extendForward();
+        }
+
+        public class ExtendBackwards implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+//                RobotConfig.slideExtension.setPower(-0.7);
+//                runtime.reset();
+//                while (opModeIsActive() && (runtime.seconds() < 2.0)) {
+//                    telemetry.addData("Path", "Extend", runtime.seconds());
+//                    telemetry.update();
+//                }
+                targetPos = 0; //make it full slide extend
+                RobotConfig.slideExtension.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                while (RobotConfig.slideExtension.getCurrentPosition() < 0) {
+                    RobotConfig.slideExtension.setPower(1);
+                }
+                RobotConfig.slideExtension.setPower(0);
+
+                return false;
+            }
+
+        }
+        public Action extendBackwards() {
+            return new extend.ExtendBackwards();
+        }
+
+    }
+
+    @Override
+    public void runOpMode() {
+        RobotConfig.initialize(hardwareMap);
+
+        Pose2d initialPose = new Pose2d(-11.5, -60, Math.toRadians(90));
+        SparkFunOTOSDrive drive = new SparkFunOTOSDrive(hardwareMap, initialPose);
+
+        // instances
+        Claw claw = new Claw();
+        Lift lift = new Lift();
+        wrist wrist = new wrist();
+        extend extend = new extend();
+
+        // align to block
+        TrajectoryActionBuilder alignPickup = drive.actionBuilder(initialPose)
+                .strafeToLinearHeading(new Vector2d(25, -67), Math.toRadians(0));
+
+        // pickup
+        TrajectoryActionBuilder moveToPickup = drive.actionBuilder(new Pose2d(25, -67, Math.toRadians(0)))
+                .strafeToLinearHeading(new Vector2d(35, -67), Math.toRadians(0));
+
+        // basket
+        TrajectoryActionBuilder toBasket = drive.actionBuilder(new Pose2d(35, -67, Math.toRadians(0)))
+                .strafeToLinearHeading(new Vector2d(-63, -59), Math.toRadians(45));
+
+        // park
+        TrajectoryActionBuilder park = drive.actionBuilder(new Pose2d(-63, -59, Math.toRadians(45)))
+                .strafeToLinearHeading(new Vector2d(35, -55), Math.toRadians(0));
+
+        // traj action close out
+        Action trajectoryActionCloseOut = alignPickup.endTrajectory().fresh()
+                .build();
+
+        // actions that need to happen on init; for instance, a claw tightening.
+        Actions.runBlocking(claw.closeClaw());
+        while (!isStopRequested() && !opModeIsActive()) {
+            telemetry.addLine("We're the goats don't worry drivers, WE ARE THEM!");
+            telemetry.addData("x", drive.pose.position.x);
+            telemetry.addData("y", drive.pose.position.y);
+            telemetry.addData("heading (deg)", Math.toDegrees(drive.pose.heading.toDouble()));
+            telemetry.update();
+        }
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Action align = alignPickup.build();
+        Action pickup = moveToPickup.build();
+        Action basket = toBasket.build();
+        Action parking = park.build();
+        Actions.runBlocking(align);
+        RobotConfig.intake.setPosition(1);
+        Actions.runBlocking(pickup);
+        RobotConfig.intake.setPosition(0.5);
+        Actions.runBlocking(basket);
+        Actions.runBlocking(lift.liftUp());
+        RobotConfig.wrist1.setPosition(0.25);
+        Actions.runBlocking(extend.extendForward());
+        RobotConfig.intake.setPosition(1);
+
+    }
+}
